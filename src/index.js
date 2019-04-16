9594491c4a4d02bca14f06ebe62fa9f455c97734
Alec Larson <alec.stanford.larson@gmail.com>
2019-04-16 15:25:36 -0400

    improve index.js

--- src/index.js
+++ src/index.js
@@ -7,20 +7,26 @@ import { Publisher } from "./publisher";
 import { History } from "./history";
 import "./styles.css";
 
+// Magical default state (observable and immutable)
 const initialState = o({
   h1: "Hello CodeSandbox",
   h2: "Start editing to see some magic happen!"
 });
 
+// Keep a reference to the latest state.
 const state = keepAlive(initialState);
 
+// Padding between UI elements.
 const spacer = <div className="spacer" />;
 
-// TODO: Create a cooler example
+/**
+ * The `App` component contains 4 types of interesting components:
+ *   - Subscriber: Listens to the entire state and updates the headings
+ *   - Publisher: An editor for a single property of the state
+ *   - History: JSON stream of 10 most recent state snapshots
+ *   - ResetButton: Reverts the state back to its first snapshot
+ */
 class App extends React.Component {
-  componentDidCatch(error) {
-    console.error(error);
-  }
   render() {
     return (
       <>
@@ -31,14 +37,25 @@ class App extends React.Component {
           {spacer}
           <Publisher name={"h2"} state={state} />
           {spacer}
-          <button onClick={() => revise(state, initialState)}>Reset</button>
+          <ResetButton from={state} to={initialState} />
           {spacer}
         </div>
         <History state={state} />
+        <div
+          className="history-item"
+          style={{ color: "#4DB038", marginTop: 20, marginBottom: 40 }}
+        >
+          Watching for changes...
+        </div>
       </>
     );
   }
 }
+
+// Magical reset
+const ResetButton = props => (
+  <button onClick={() => revise(props.from, props.to)}>Reset</button>
+);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
